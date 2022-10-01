e2c61cc85bcbaeecc2e58cae8d8d5e1963211dea
AlekseySav <alex2003s@yandex.ru>
2022-10-01 15:10:37 +0300

    fixed cdfa-re (now slow though)

--- src/cdfa-re.cpp
+++ src/cdfa-re.cpp
@@ -1,12 +1,21 @@
 #include "automata.h"
 
+struct edge {
+    int from, to;
+    std::string re;
+    bool deleted;
+};
+
+static std::vector<edge> edges;
+
 static struct ReDFA {
     std::vector<std::unordered_map<int, std::string>> nodes;
 } regex;
 
 static Automata a;
 
-static std::vector<bool> used;
+static std::vector<bool> g_used;
+static std::vector<std::unordered_map<int, bool>> r_used;
 static std::vector<bool> garbage;
 
 std::string star(std::string a) {
@@ -16,8 +25,8 @@ std::string star(std::string a) {
 }
 
 std::string cat(std::string a, std::string b) {
-    if (a.contains('+')) a = '(' + a + ')';
-    if (b.contains('+')) b = '(' + b + ')';
+    if (a.contains('+') && b.size()) a = '(' + a + ')';
+    if (b.contains('+') && a.size()) b = '(' + b + ')';
     if (a.size() && b.size()) return a + '.' + b;
     return a + b;
 }
@@ -29,35 +38,52 @@ void add(std::string& a, const std::string& b) {
 
 void garbage_dfs(int n) {
     int res = 0;
-    used[n] = true;
+    g_used[n] = true;
     for (auto&[v, re] : regex.nodes[n]) {
         if (v == n) continue;
-        if (!used[v]) garbage_dfs(v);
+        if (!g_used[v]) garbage_dfs(v);
         if (!garbage[v]) res++;
     }
-    if (!res && !a.F.contains(n))
+    if (!res && n != a.size())
         garbage[n] = true;
 }
 
-
-void re_dfs(int n, int prev = -1) {
-    used[n] = true;
+/* aaaaa need not dfs but pass through all edges */
+/*void re_dfs(int n, int prev = -1) {
+    if (prev >= 0) r_used[prev][n] = true;
     if (garbage[n]) return;
 
-    auto copy = regex.nodes[n];
+    std::unordered_map<int, std::string> copy = regex.nodes[n];
     for (auto&[to, re] : copy)
-        if (!used[to])
+        if (!r_used[n][to])
             re_dfs(to, n);
     for (auto&[to, re] : regex.nodes[n]) {
         if (to == n || prev < 0 || garbage[to]) continue;
         add(regex.nodes[prev][to], cat(cat(regex.nodes[prev][n], star(regex.nodes[n][n])), re));
-        regex.nodes[prev].erase(n);
+        if (!a.F.contains(n)) regex.nodes[prev].erase(n);
     }
+}*/
+
+void build_regex() {
+    for (int from = 0; from < regex.nodes.size(); from++)
+        for (auto&[to, re] : regex.nodes[from])
+            edges.emplace_back(from, to, re, false);
+    for (int i = 0; i < edges.size(); i++) { /* can't use iterators */
+        auto&[from, mid, re1, del] = edges[i];
+        if (garbage[mid] || garbage[from] || from == mid) continue;
+        for (auto&[to, re2] : regex.nodes[mid]) {
+            if (garbage[to] || to == mid) continue;
+            add(regex.nodes[from][to], cat(cat(re1, star(regex.nodes[mid][mid])), re2));
+            edges.emplace_back(from, to, regex.nodes[from][to], false);
+            del = true;
+        }
+    }
+    /*std::cout << edges.size() << '\n';*/
 }
 
 int main() {
     a.deserialize();
-    regex.nodes.resize(a.size());
+    regex.nodes.resize(a.size() + 1);
     for (int n = 0; n < a.size(); n++) {
         auto& dest = regex.nodes[n];
         for (auto&[c, to] : a.nodes[n]) {
@@ -67,18 +93,14 @@ int main() {
         }
         if (!dest.contains(n)) dest.emplace(n, "");
     }
-    used.resize(a.size());
+    for (int i : a.F)
+        regex.nodes[i][a.size()] = "";
+
+    g_used.resize(a.size());
+    r_used.resize(a.size());
     garbage.resize(a.size());
     garbage_dfs(a.q0);
-    used.assign(a.size(), false);
-    re_dfs(a.q0);
-    std::string result;
-    for (auto& v_id : a.F) {
-        const std::string& re_loop = regex.nodes[v_id][v_id];
-        const std::string& re = v_id == a.q0 ? "" : regex.nodes[a.q0][v_id];
-        add(result, re + star(re_loop));
-        if (!re_loop.size() && v_id == a.q0)
-            add(result, "0");
-    }
+    build_regex();
+    std::string result = regex.nodes[a.q0][a.size()];
     std::cout << result << '\n';
 }
